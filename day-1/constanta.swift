90d7a69cf6bcebb707a2489be4ac4be7b2d4757d
Burdye <kadekadiyamikaa@gmail.com>
2026-05-01 17:26:46 +0800

    final day-1

---
+++ day-1/constanta.swift
@@ -0,0 +1,26 @@
+// Cara membuat string
+let quote = "Swift adalah bahasa pemrograman yang menyenangkan!"
+
+let pesan = "learning \"dff\"fdffdf"
+
+let message = #"hello "fgffd" dfdf"#
+
+let multiMessage = """
+    hello
+    this is
+    multi 
+    message
+    """
+
+// Function
+
+print(quote.uppercased())
+print(quote.lowercased())
+print(quote.count)
+print(quote.hasPrefix("Swift"))
+print(quote.hasSuffix("!"))
+
+
+print(pesan)
+print(message)
+print(multiMessage)
---
+++ day-1/storeWholeNumber.swift
@@ -0,0 +1,5 @@
+// How to store whole numbers
+let number = 100_000___000
+print(number)
+
+print(number.isMultiple(of:2))
