8b24ff23380d2fee409bce0d7267ac0f38606b59
Burdye <kadekadiyamikaa@gmail.com>
2026-05-01 15:45:11 +0800

    Menambahkan variable dan konstanta

---
+++ day-0/1.swift
@@ -0,0 +1,11 @@
+// How to create variables and constants
+
+var huruf = "A"
+huruf = "B"
+
+print(huruf)
+
+
+// constants
+let angka = 1
+print(angka)
--- day-0/hello.swift
+++
@@ -1,2 +0,0 @@
-print("asdsfsafafa")
-print("percobaan commit")
