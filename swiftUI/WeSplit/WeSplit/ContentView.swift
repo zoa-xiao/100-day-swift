d6b78463d78d26cad6b200cbc725193dc141d994
Burdye <kadekadiyamikaa@gmail.com>
2026-05-30 18:57:24 +0800

    final project 1

--- swiftUI/WeSplit/WeSplit/ContentView.swift
+++ swiftUI/WeSplit/WeSplit/ContentView.swift
@@ -8,10 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body : some View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused : Bool
+    
+    let tipPercentages = [10,15,20,25,0]
+    var totals: (amountPerPerson : Double, grandTotal : Double) {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerson = grandTotal / peopleCount
+        
+        return (amountPerson,  grandTotal)
         
     }
-    
+   
+    var body : some View {
+        NavigationStack{
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code : Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<5) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                
+                
+                Section("How much do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(1..<101) {
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                Section("Amount per person") {
+                    Text(totals.amountPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Grand Total") {
+                    Text(totals.grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+               
+            }.navigationTitle("wesplit")
+            
+                .toolbar {
+                    if amountIsFocused {
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+        }
+    }
     
 }
 
