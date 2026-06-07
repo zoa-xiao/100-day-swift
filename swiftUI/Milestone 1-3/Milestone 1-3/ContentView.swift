383a505edf7d5ccdfc19e810448100e335d84f19
Burdye <kadekadiyamikaa@gmail.com>
2026-06-07 22:15:33 +0800

    milestone 1-3

---
+++ swiftUI/Milestone 1-3/Milestone 1-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone 1-3
+//
+//  Created by Adi Yamika on 07/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI/Milestone 1-3/Milestone 1-3/Milestone_1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_1_3App.swift
+//  Milestone 1-3
+//
+//  Created by Adi Yamika on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
